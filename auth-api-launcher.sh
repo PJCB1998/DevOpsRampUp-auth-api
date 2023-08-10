9373b645e62c8186bfa44351c3f65c84e14a72ac
Pablo Calle <pjcb1998@hotmail.com>
2023-08-10 14:39:26 -0500

    Update auth-api-launcher.sh

--- auth-api-launcher.sh
+++ auth-api-launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash 
 
-JWT_SECRET=PRFT AUTH_API_PORT=8000 USERS_API_ADDRESS=http://127.0.0.1:8083 /app/auth-api/auth-api > /var/log/auth-api_log
+JWT_SECRET=PRFT AUTH_API_PORT=8000 USERS_API_ADDRESS=http://192.168.56.30:8083 ZIPKIN_URL=http://192.168.56.80:9411/api/v2/spans /app/auth-api/auth-api > /var/log/auth-api_log
